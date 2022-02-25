ad75121ee57d1a505aa0a2d9a5ebdbf1118feba1
Aibek Ramazanov <aibekramazanov@Aibeks-MBP.lan>
2022-02-25 18:54:49 -0500

    pagination project

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,14 @@
-const paginate = () => {}
+const paginate = (followers) => {
+    const itemsPerPage = 9;
+    const pages = Math.ceil(followers.length / itemsPerPage);
 
-export default paginate
+    const newFollowers = Array.from({ length: pages }, (_, index) => {
+        const start = index * itemsPerPage;
+        console.log(start);
+        return followers.slice(start, start + itemsPerPage);
+    });
+    console.log(newFollowers);
+    return newFollowers;
+};
+
+export default paginate;
